8c042897989e8272fc0a72632d82e4f501c3e649
cheungyik <147300498+cheungyik@users.noreply.github.com>
2025-08-02 12:12:50 +0800

    feat: enhance Query function to return Mobiledom with brand identification

--- mobiledom.go
+++ mobiledom.go
@@ -2,6 +2,8 @@ package mobiledom
 
 import (
 	"encoding/json"
+	"regexp"
+	"strings"
 )
 
 var mobiles = make(map[string]*Mobiledom)
@@ -17,5 +19,26 @@ func init() {
 }
 
 func Query(model string) *Mobiledom {
-	return mobiles[model]
+	mobiledom := mobiles[model]
+	if mobiledom != nil {
+		return mobiledom
+	}
+	return &Mobiledom{
+		Brand: func() string {
+			switch regexp.MustCompile(`[0-9]`).ReplaceAllString(strings.ToLower(strings.SplitN(model, " ", 2)[0]), "") {
+			case "vivo":
+				return "vivo 手机"
+			case "mi", "redmi":
+				return "小米手机"
+			case "meizu":
+				return "魅族手机"
+			case "oppo":
+				return "OPPO 手机"
+			default:
+				return "UNKNOWN"
+			}
+		}(),
+		ProductName:  model,
+		SalesVersion: model,
+	}
 }
